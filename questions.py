18a5fbd2f7b16d18d44ab605889f9fcd59779d70
Dmitriy <levchenko.d.a1998@gmail.com>
2021-02-18 16:38:58 +0300

    Update questions.py

--- questions.py
+++ questions.py
@@ -1,160 +1,118 @@
+"""Classes for creating Questions, States with Questions and Conversation States Groups."""
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from dataclasses import field
-from typing import TypeVar, Union, Optional, Literal
+from typing import Union, Callable, Awaitable, Optional
 
-from aiogram import types, Dispatcher, Bot
-from aiogram.contrib.questions import ConvState, ConvStatesGroup
-from aiogram.contrib.questions import Quest, QuestText, QuestFunc
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.middlewares import BaseMiddleware
+from aiogram import types, Dispatcher
+from aiogram.dispatcher.filters.state import State, StatesGroupMeta, StatesGroup
 
-# выход после обработки, выход без обработки, выход с исключением
-
-
-__all__ = ['UserDataUpdater', 'SwitchConvState', 'get_current_state', 'HandleException', 'NewData']
-
-T = TypeVar('T')
-StorageData = Union[str, int, list, None]
-
-
-def to_list(obj) -> list:
-    if not isinstance(obj, list):
-        obj = [obj]
-    return obj
-
-
-def search_in_results(obj_type: type[T], container: list) -> Optional[T]:
-    """Recursive search for instance of obj_type in lists/tuples."""
-    if isinstance(container, (list, tuple)):
-        for item in container:
-            obj = search_in_results(obj_type, item)
-            if obj is not None:  # object found
-                return obj
-    else:
-        if isinstance(container, obj_type):
-            return container
-
-
-async def ask_question(question: Union[Quest, list[Quest]]):
-    """Send message for each Quest in question [current Chat]."""
-    chat = types.Chat.get_current()
-    bot = Bot.get_current()
-
-    async def ask_quest(quest: Quest):
-        if isinstance(quest, str):
-            await bot.send_message(chat.id, quest)
-        elif isinstance(quest, QuestText):
-            await bot.send_message(chat.id, quest.text, reply_markup=quest.keyboard)
-        elif isinstance(quest, QuestFunc):
-            await quest.async_func()
-
-    for q in to_list(question):
-        await ask_quest(q)
+KeyboardMarkup = Union[types.ReplyKeyboardMarkup, types.InlineKeyboardMarkup]
+AsyncFunction = Callable[[], Awaitable]
 
 
 @dataclass
-class HandleException:
-    on_exception: Quest = None
+class QuestText:
+    """Text and keyboard for an ordinary question."""
+    text: str
+    keyboard: KeyboardMarkup
 
 
 @dataclass
-class NewData:
-    set_items: dict[str, StorageData] = field(default_factory=dict)
-    extend_items: dict[str, StorageData] = field(default_factory=dict)
-    del_keys: Union[str, list] = field(default_factory=list)
-
-    async def update_proxy(self, state_ctx: FSMContext):
-        """Set, extend or delete items with state_ctx.proxy()."""
-        async with state_ctx.proxy() as udata:
-            udata.update(self.set_items)
-
-            for key, value in self.extend_items.items():
-                udata.setdefault(key, [])
-                udata[key].extend(to_list(value))
-
-            for key in to_list(self.del_keys):
-                del udata[key]
+class QuestFunc:
+    """Async function for an extraordinary question. Will be called without args."""
+    async_func: AsyncFunction
 
 
-@dataclass
-class NewState:
-    conv_state: Union[ConvState, type[ConvStatesGroup]] = 'previous'
-    on_conv_exit: Quest = None
-
-    async def set_state(self) -> ConvState:
-        if isinstance(self.conv_state, ConvState):
-            state = self.conv_state
-        elif isinstance(self.conv_state, type(ConvStatesGroup)):
-            self.conv_state: type[ConvStatesGroup]
-            state = self.conv_state.states[0]
-        elif self.conv_state == 'previous':
-            state = await ConvStatesGroup.get_previous_state()
-        else:
-            state = None
-
-        if state:
-            await state.set()
-
-        return state
+Quest = Union[str, QuestText, QuestFunc, None]
+Quests = Union[Quest, list[Quest]]
 
 
-class PostMiddleware(BaseMiddleware, ABC):
-    """Abstract Middleware for post processing Message and CallbackQuery."""
+class ConvState(State):
+    """State with question attribute. It should be used to ask next question in conversation."""
 
-    @classmethod
-    @abstractmethod
-    async def on_post_process_message(cls, msg: types.Message, results: list, state_dict: dict):
-        """Works after processing any message by handler."""
-
-    @classmethod
-    async def on_post_process_callback_query(cls, query: types.CallbackQuery, results: list, state_dict: dict):
-        """Answer query [empty text] and call on_post_process_message(query.message)."""
-        await query.answer()
-        await cls.on_post_process_message(query.message, results, state_dict)
+    def __init__(self, question: Quests):
+        self.question = question
+        super().__init__()
 
 
-class UserDataUpdater(PostMiddleware):
-    """Search for NewData(...) in handle results. Update storage for current User+Chat with new data."""
+class ConvStatesGroupMeta(StatesGroupMeta):
+    """Check if StatesGroup have only ConvState(...) attributes (not State)."""
 
-    @classmethod
-    async def on_post_process_message(cls, msg: types.Message, results: list, *args):
-        state_ctx = Dispatcher.get_current().current_state()
-        new_data = search_in_results(NewData, results)
-        if new_data:
-            await new_data.update_proxy(state_ctx)
+    def __new__(mcs, class_name, bases, namespace, **kwargs):
+        for prop in namespace.values():
+            if isinstance(prop, State) and not isinstance(prop, ConvState):
+                err_text = f'{class_name} attrs must be instance of {ConvState.__name__}, not {State.__name__}'
+                raise TypeError(err_text)
+
+        return super().__new__(mcs, class_name, bases, namespace)
+
+    def all_conv_states(cls) -> list[ConvState]:
+        """Search for all ConvState(...) in all sublasses."""
+        all_states = []
+        for conv_group in cls.__subclasses__():
+            all_states.extend(conv_group.all_states)
+        return all_states
+
+    def get_state_by_name(cls, state_name: str) -> Optional[ConvState]:
+        """Search for State with state_name in subclasses."""
+        for state in cls.all_conv_states():
+            if state.state == state_name:
+                return state
+
+    async def get_current_state(cls) -> Optional[ConvState]:
+        try:
+            state_ctx = Dispatcher.get_current().current_state()
+            state_name = await state_ctx.get_state()
+            state = cls.get_state_by_name(state_name)
+            return state
+        except AttributeError:
+            return None
+
+    async def get_next_state(cls) -> Optional[ConvState]:
+        state = await cls.get_current_state()
+
+        try:
+            group_states: tuple[ConvState] = state.group.states
+        except AttributeError:
+            return None
+
+        next_step = group_states.index(state) + 1
+        return cls.get_state_by_index(group_states, next_step)
+
+    async def get_previous_state(cls) -> Optional[ConvState]:
+        state = await cls.get_current_state()
+
+        try:
+            group_states: tuple[ConvState] = state.group.states
+        except AttributeError:
+            return None
+
+        previous_step = group_states.index(state) - 1
+        return cls.get_state_by_index(group_states, next_step)
+
+    async def __get_first_group_state(cls) -> Optional[ConvState]:
+        state = await cls.get_current_state()
+
+        try:
+            group_states: tuple[ConvState] = state.group.states
+            return group_states[0]
+        except AttributeError:
+            return None
+
+    async def __get_last_group_state(cls) -> Optional[ConvState]:
+        state = await cls.get_current_state()
+
+        try:
+            group_states: tuple[ConvState] = state.group.states
+            return group_states[-1]
+        except AttributeError:
+            return None
+
+    def get_state_by_index(cls, group_states: tuple[ConvState], index: int) -> Optional[ConvState]:
+        if 0 <= index < len(group_states):
+            return group_states[index]
 
 
-class SwitchConvState(PostMiddleware):
-    """Switch state for current User+Chat [if user is in conversation].
-
-    # If HandleException in handle results - process exception;
-    # Else if ConvStateGroup in handle results - set first state in group;
-    # Else if user is in conversation - set next state in group;
-    """
-
-    @classmethod
-    async def on_post_process_message(cls, msg: types.Message, results: list, *args):
-        exception = search_in_results(HandleException, results)
-        new_state = search_in_results(NewState, results)
-
-        if exception:
-            await ask_question(exception.on_exception)
-
-        elif new_state:
-            next_state = await new_state.set_state()
-            if next_state:
-                await ask_question(next_state.question)
-            else:
-                await ask_question(new_state.on_conv_exit)
-
-        else:
-            next_state: ConvState = await ConvStatesGroup.get_next_state()
-            if next_state:
-                await next_state.set()
-                await ask_question(next_state.question)
-            else:
-                state_ctx = Dispatcher.get_current().current_state()
-                await state_ctx.finish()
+class ConvStatesGroup(StatesGroup, metaclass=ConvStatesGroupMeta):
+    """StatesGroup with only ConvState(...) attributes (not State)."""
